7587246671340b683a0b8f06fb369d6574d8274f
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2022-08-10 08:38:50 +0200

    Version 0.2: Support for MSVC 2005 to MSVC 2022
    - The passed command line now matches Windows standards.
    - MSVC 2005 and up are now detected and can be installed.
    - Optimized the code slightly with more C++-like handling.
    - Merged library lists for 2015, 2017, 2019 and 2022.
    - Extended library lists with most of the known universal libraries.
    - Removed pointless self-signed code certificate.

--- source/bin.cpp
+++ source/bin.cpp
@@ -11,6 +11,7 @@
 #include <list>
 #include <map>
 #include <memory>
+#include <set>
 #include <sstream>
 #include <string_view>
 #include <vector>
@@ -20,53 +21,358 @@
 
 #include "curl.hpp"
 
-#ifdef _WIN64
-const std::map<int32_t, std::string_view> redist_urls = {
-	{2015, "https://aka.ms/vs/16/release/vc_redist.x64.exe"},
-	{2017, "https://aka.ms/vs/16/release/vc_redist.x64.exe"},
-	{2019, "https://aka.ms/vs/16/release/vc_redist.x64.exe"},
-};
-#else
-const std::map<int32_t, std::string_view> redist_urls = {
-	{2015, "https://aka.ms/vs/16/release/vc_redist.x86.exe"},
-	{2017, "https://aka.ms/vs/16/release/vc_redist.x86.exe"},
-	{2019, "https://aka.ms/vs/16/release/vc_redist.x86.exe"},
-};
+#if defined(__x86_64__) || defined(_M_X64)
+#define IS_X86
+#define IS_64BIT
+#elif defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
+#define IS_X86
+#define IS_32BIT
+#elif defined(__aarch64__) || defined(_M_ARM64)
+#define IS_ARM
+#define IS_64BIT
+#elif defined(__arm__) || defined(_M_ARM)
+#define IS_ARM
+#define IS_32BIT
 #endif
 
-const std::map<int32_t, std::list<std::string_view>> redist_dlls = {
-	{2015,
-	 {
-		 "msvcp140.dll",
-		 "vcruntime140.dll",
-	 }},
-	{2017,
-	 {
-		 "msvcp140.dll",
-		 "vcruntime140.dll",
-		 "msvcp140_1.dll",
-		 "vcruntime140_1.dll",
-	 }},
-	{2019,
-	 {
-		 "msvcp140.dll",
-		 "vcruntime140.dll",
-		 "msvcp140_1.dll",
-		 "vcruntime140_1.dll",
-		 "msvcp140_2.dll",
-	 }},
+struct redists {
+	std::set<int32_t>                              versions;
+	std::set<std::filesystem::path>                libs;
+	std::list<std::pair<std::string, std::string>> urls;
 };
+
+const std::list<redists> known_redists = {
+	{
+		// 80
+		{2005},
+		{
+			//			"atl80.dll",
+			"mfc80.dll",
+			"mfc80u.dll",
+			"mfcm80.dll",
+			"mfcm80u.dll",
+			"msvcm80.dll",
+			"msvcp80.dll",
+			"msvcr80.dll",
+			"vcomp80.dll",
+		},
+		{
+#if defined(IS_X86) && defined(IS_64BIT)
+			{"https://download.microsoft.com/download/8/B/4/8B42259F-5D70-43F4-AC2E-4B208FD8D66A/vcredist_x64.EXE",
+			 "/Q"},
+#elif defined(IS_X86) && defined(IS_32BIT)
+			{"https://download.microsoft.com/download/8/B/4/8B42259F-5D70-43F4-AC2E-4B208FD8D66A/vcredist_x86.EXE",
+			 "/Q"},
+#endif
+		},
+	},
+	{
+		// 90
+		{2008},
+		{
+			//			"atl90.dll",
+			"mfc90.dll",
+			"mfc90u.dll",
+			"mfcm90.dll",
+			"mfcm90u.dll",
+			"msvcp90.dll",
+			"msvcr90.dll",
+			"vcomp90.dll",
+		},
+		{
+#if defined(IS_X86) && defined(IS_64BIT)
+			{"https://download.microsoft.com/download/5/D/8/5D8C65CB-C849-4025-8E95-C3966CAFD8AE/vcredist_x64.exe",
+			 "/q"},
+#elif defined(IS_X86) && defined(IS_32BIT)
+			{"https://download.microsoft.com/download/5/D/8/5D8C65CB-C849-4025-8E95-C3966CAFD8AE/vcredist_x86.exe",
+			 "/q"},
+#endif
+		},
+	},
+	{
+		// 100
+		{2010},
+		{
+			"atl100.dll",
+			"mfc100.dll",
+			"mfc100u.dll",
+			"mfcm100.dll",
+			"mfcm100u.dll",
+			"mrt100.dll",
+			"msvcp100.dll",
+			"msvcr100.dll",
+			"pdmproxy100.dll",
+			"vcomp100.dll",
+		},
+		{
+#if defined(IS_X86) && defined(IS_64BIT)
+			{"https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/vcredist_x64.exe",
+			 "/install /quiet /norestart"},
+#elif defined(IS_X86) && defined(IS_32BIT)
+			{"https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/vcredist_x86.exe",
+			 "/install /quiet /norestart"},
+#endif
+		},
+	},
+	{
+		// 110
+		{2012},
+		{
+			"atl110.dll",
+			"mfc110.dll",
+			"mfc110u.dll",
+			"mfcm110.dll",
+			"mfcm110u.dll",
+			"msvcp110.dll",
+			"msvcr110.dll",
+			"vcamp110.dll",
+			"vccorlib110.dll",
+			"vcomp110.dll",
+		},
+		{
+#if defined(IS_X86) && defined(IS_64BIT)
+			{"https://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x64.exe",
+			 "/install /quiet /norestart"},
+#elif defined(IS_X86) && defined(IS_32BIT)
+			{"https://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_x86.exe",
+			 "/install /quiet /norestart"},
+#elif defined(IS_ARM) && defined(IS_32BIT)
+			{"https://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU_4/vcredist_arm.exe",
+			 "/install /quiet /norestart"},
+#endif
+		},
+	},
+	{
+		// 120
+		{2013},
+		{
+			"mfc120.dll",
+			"mfc120u.dll",
+			"mfcm120.dll",
+			"mfcm120u.dll",
+			"msvcp120.dll",
+			"msvcr120.dll",
+			"vcamp120.dll",
+			"vccorlib120.dll",
+			"vcomp120.dll",
+		},
+		{
+#if defined(IS_X86) && defined(IS_64BIT)
+			{"https://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x64.exe",
+			 "/install /quiet /norestart"},
+#elif defined(IS_X86) && defined(IS_32BIT)
+			{"https://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x86.exe",
+			 "/install /quiet /norestart"},
+#elif defined(IS_ARM) && defined(IS_32BIT)
+			{"https://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_arm.exe",
+			 "/install /quiet /norestart"},
+#endif
+		},
+	},
+	{
+		// 140
+		{2015, 2017, 2019, 2022},
+		{
+			"concrt140.dll",
+#if defined(IS_X86) && defined(IS_64BIT)
+			"libomp140.x86_64.dll",
+#elif defined(IS_X86) && defined(IS_32BIT)
+			"libomp140.x86.dll",
+#elif defined(IS_ARM) && defined(IS_64BIT)
+			"libomp140.arm64.dll",
+#endif
+			"mfc140.dll",
+			"mfc140u.dll",
+			"mfcm140.dll",
+			"mfcm140u.dll",
+			"msvcp140.dll",
+			"msvcp140_1.dll",
+			"msvcp140_2.dll",
+			"msvcp140_atomic_wait.dll",
+			"msvcp140_codecvt_ids.dll",
+			"vcamp140.dll",
+			"vccorlib140.dll",
+			"vcomp140.dll",
+			"vcruntime140.dll",
+			"vcruntime140_1.dll",
+		},
+		{
+#if defined(IS_X86) && defined(IS_64BIT)
+			{"https://aka.ms/vs/17/release/vc_redist.x64.exe", "/install /quiet /norestart"},
+#elif defined(IS_X86) && defined(IS_32BIT)
+			{"https://aka.ms/vs/17/release/vc_redist.x86.exe", "/install /quiet /norestart"},
+#elif defined(IS_ARM) && defined(IS_64BIT)
+			{"https://aka.ms/vs/17/release/vc_redist.arm64.exe", "/install /quiet /norestart"},
+#endif
+		},
+	},
+};
+
+std::string format_string(std::string_view format, ...)
+{
+	std::vector<char> buffer;
+	va_list           args1;
+	va_list           args2;
+
+	va_start(args1, format);
+	int sz = vsnprintf(nullptr, 0, format.data(), args1);
+	va_end(args1);
+
+	if (sz == -1) {
+		throw std::runtime_error("Failed to format string.");
+	}
+
+	buffer.resize(sz + 1);
+	if (buffer.size() == 0) {
+		throw std::runtime_error("Failed to reserve memory.");
+	}
+
+	va_start(args2, format);
+	vsnprintf(buffer.data(), buffer.size(), format.data(), args2);
+	va_end(args2);
+
+	return std::string{buffer.begin(), buffer.end() - 1};
+}
+
+std::wstring widen(std::string str)
+{
+	std::vector<WCHAR> buffer;
+
+	int size = MultiByteToWideChar(CP_UTF8, DWORD(0), str.data(), static_cast<int>(str.size()), NULL, int(0));
+	buffer.resize(static_cast<size_t>(size) + 1);
+	MultiByteToWideChar(CP_UTF8, DWORD(0), str.data(), static_cast<int>(str.size()), buffer.data(),
+						static_cast<int>(buffer.size()));
+	return {buffer.begin(), buffer.end() - 1};
+}
 
 static void helper_free_handle(void* handle)
 {
 	CloseHandle(static_cast<HANDLE>(handle));
 }
 
+void download_file(const std::string& url, const std::filesystem::path& file)
+{
+	std::ofstream stream{file, std::ios::out | std::ios::binary};
+	if (!stream.is_open() || stream.bad()) {
+		throw std::runtime_error(format_string("Failed to open file '%s'.", file.string().c_str()));
+	}
+
+	util::curl curl;
+	curl.set_option(CURLOPT_HTTPGET, 1);
+	curl.set_option(CURLOPT_URL, url);
+	curl.set_option(CURLOPT_FOLLOWLOCATION, 1);
+	curl.set_option(CURLOPT_CRLF, 0);
+	curl.set_write_callback([&stream](void* data, size_t bytes, size_t count) {
+		stream.write(static_cast<char*>(data), bytes * count);
+		return bytes * count;
+	});
+	curl.set_xferinfo_callback([](uint64_t total, uint64_t now, uint64_t, uint64_t) { return int32_t(0); });
+
+	std::cout << "Downloading '" << url << "'..." << std::endl;
+	CURLcode res = curl.perform();
+	stream.close();
+
+	if (res != CURLE_OK) {
+		std::filesystem::remove(file);
+		throw std::runtime_error(
+			format_string("Failed to download file '%s' from '%s'.", file.string().c_str(), url.c_str()));
+	}
+}
+
+void launch_file(const std::filesystem::path& file, std::string command_line_u8)
+{
+	std::cout << format_string("Launching '%s' with command line '%s'...", file.u8string().c_str(),
+							   command_line_u8.c_str())
+			  << std::endl;
+
+	// Build an acceptable command line ([0] = module, [1...] = arguments)
+	std::wstring       module_name = file.wstring();
+	std::vector<WCHAR> command_line;
+	{
+		std::wstringstream wstr;
+		wstr << L"\"" << module_name << L"\"" << L" ";
+		wstr << widen(command_line_u8);
+
+		std::wstring raw = wstr.str();
+		command_line     = {raw.begin(), raw.end()};
+		command_line.push_back(0);
+	}
+
+	PROCESS_INFORMATION pi{0};
+
+	STARTUPINFOW si{0};
+	si.cb = sizeof(STARTUPINFOW);
+
+	SECURITY_ATTRIBUTES sa{0};
+	sa.nLength        = sizeof(SECURITY_ATTRIBUTES);
+	sa.bInheritHandle = TRUE;
+
+	std::shared_ptr<void> managed_process_handle;
+	std::shared_ptr<void> managed_thread_handle;
+	BOOL res = CreateProcessW(module_name.data(), command_line.data(), &sa, &sa, TRUE, CREATE_UNICODE_ENVIRONMENT, NULL,
+							  NULL, &si, &pi);
+	managed_process_handle = std::shared_ptr<void>(static_cast<void*>(pi.hProcess), helper_free_handle);
+	managed_thread_handle  = std::shared_ptr<void>(static_cast<void*>(pi.hThread), helper_free_handle);
+
+	if (!res) {
+		DWORD error = GetLastError();
+		throw std::runtime_error(
+			format_string("Failed to launch '%s', error code: %ld.", file.string().c_str(), error));
+	}
+
+	while (WaitForSingleObject(pi.hProcess, 1000) == WAIT_TIMEOUT) {
+		std::cout << ".";
+		std::cout.flush();
+	}
+	std::cout << std::endl;
+
+	DWORD exitcode = 0;
+	GetExitCodeProcess(pi.hProcess, &exitcode);
+	if (exitcode != 0) {
+		throw std::runtime_error("Installing Redistributable failed.");
+	}
+}
+
+void install_libraries(int32_t version, const std::list<std::pair<std::string, std::string>>& urls)
+{
+	std::filesystem::path file;
+	file = std::filesystem::temp_directory_path();
+	file /= format_string("msvc-redist-%ld-%lld.exe", version, static_cast<int64_t>(time(NULL)));
+
+	std::cout << "Installing Microsoft Visual C/C++ Redistributable version " << version << "..." << std::endl;
+	for (const auto& kv : urls) {
+		try {
+			download_file(kv.first, file);
+			launch_file(file, kv.second);
+		} catch (const std::exception& ex) {
+			if (std::filesystem::exists(file)) {
+				std::filesystem::remove(file);
+			}
+			throw ex;
+		}
+	}
+}
+
+bool test_libraries(const std::set<std::filesystem::path>& libs)
+{
+	for (auto& lib : libs) {
+		auto    lib_name   = lib.wstring();
+		HMODULE lib_handle = LoadLibraryW(lib_name.data());
+		auto    lib_ref    = std::shared_ptr<void>(static_cast<void*>(lib_handle),
+                                             [](void* ptr) { FreeLibrary(static_cast<HMODULE>(ptr)); });
+		if (!lib_ref) {
+			std::cout << format_string("Missing library '%s'.", lib.string().c_str()) << std::endl;
+			return false;
+		}
+	}
+	return true;
+}
+
 int32_t main(int32_t argc, const char* argv[])
 {
 	// Args:
 	// [0] binary
-	// [1] msvc version to test and download.
+	// [1] Microsoft Visual C/C++ Redistributable version to test and install.
 	// >=[2] error out.
 
 	if (argc == 1) {
@@ -78,130 +384,33 @@ int32_t main(int32_t argc, const char* argv[])
 		return 1;
 	}
 
-	int32_t msvc_version = atoi(argv[1]);
-
-	auto url_found = redist_urls.find(msvc_version);
-	if (url_found == redist_urls.end()) {
-		std::cerr << "The selected MSVC version has no known download URLs." << std::endl;
-		return 1;
-	}
-
-	auto dlls_found = redist_dlls.find(msvc_version);
-	if (dlls_found == redist_dlls.end()) {
-		std::cerr << "The selected MSVC version has no known DLLs and can't be tested for." << std::endl;
-		return 1;
-	}
-
-	std::cout << "Checking if all necessary DLLs for MSVC version " << msvc_version << " exist..." << std::endl;
-
-	bool missing_dll = false;
-	for (auto dll : dlls_found->second) {
-		std::vector<WCHAR> buf;
-
-		int size = MultiByteToWideChar(CP_UTF8, DWORD(0), dll.data(), static_cast<int>(dll.size()), NULL, int(0));
-		buf.resize(static_cast<size_t>(size) + 1);
-		MultiByteToWideChar(CP_UTF8, DWORD(0), dll.data(), static_cast<int>(dll.size()), buf.data(),
-							static_cast<int>(buf.size()));
-
-		HMODULE libh = LoadLibraryW(buf.data());
-		auto    lib =
-			std::shared_ptr<void>(static_cast<void*>(libh), [](void* ptr) { FreeLibrary(static_cast<HMODULE>(ptr)); });
-		if (lib) {
-			std::cout << "  " << dll << ": Found" << std::endl;
-		} else {
-			std::cout << "  " << dll << ": Missing" << std::endl;
-			missing_dll = true;
+	// Find the matching Microsoft Visual C/C++ Redistributable.
+	int32_t msvc_version = atoi(argv[1]); // Unsafe.
+	bool    known        = false;
+	for (const auto& kv : known_redists) {
+		// Is this the correct entry?
+		if (auto v = kv.versions.find(msvc_version); v == kv.versions.end()) {
+			// No, so we skip it.
+			continue;
 		}
-	}
+		std::cout << "Checking if all necessary DLLs for MSVC version " << msvc_version << " exist..." << std::endl;
 
-	if (!missing_dll) {
-		std::cout << "Installed MSVC redistributable is up to date." << std::endl;
-		return 0;
-	}
-
-	std::filesystem::path path = std::filesystem::temp_directory_path();
-	{
-		std::cout << "Missing required DLLs, downloading installer..." << std::endl;
-
-		// Open a temporary file.
-		std::ofstream file;
-		{
-			std::stringstream sstr;
-			sstr << "ms-vcredist-" << msvc_version << "-" << static_cast<int64_t>(time(NULL)) << ".exe";
-			path.concat(sstr.str().c_str());
-
-			file.open(path, std::ios::out | std::ios::binary);
-			if (!file.is_open() || file.bad()) {
-				std::cerr << "Failed to create installer file." << std::endl;
+		if (test_libraries(kv.libs)) {
+			std::cout << "Installed MSVC redistributable is up to date." << std::endl;
+			return 0;
+		} else {
+			try {
+				install_libraries(msvc_version, kv.urls);
+				std::cout << "Successfully updated MSVC " << msvc_version << " Redistributable to latest version."
+						  << std::endl;
+				return 0;
+			} catch (const std::exception& ex) {
+				std::cerr << ex.what() << std::endl;
 				return 1;
 			}
 		}
-
-		util::curl curl;
-		curl.set_option(CURLOPT_HTTPGET, 1);
-		curl.set_option(CURLOPT_URL, url_found->second);
-		curl.set_option(CURLOPT_FOLLOWLOCATION, 1);
-		curl.set_option(CURLOPT_CRLF, 0);
-		curl.set_write_callback([&file](void* buf, size_t n, size_t c) {
-			file.write(static_cast<char*>(buf), n * c);
-			return n * c;
-		});
-		curl.set_xferinfo_callback([](uint64_t total, uint64_t now, uint64_t, uint64_t) { return int32_t(0); });
-		CURLcode res = curl.perform();
-		file.close();
-		if (res != CURLE_OK) {
-			std::cerr << "Failed to download installer." << std::endl;
-			return 1;
-		}
 	}
 
-	{
-		PROCESS_INFORMATION   pi{0};
-		STARTUPINFOW          si{0};
-		SECURITY_ATTRIBUTES   sa{0};
-		std::shared_ptr<void> processh;
-		std::shared_ptr<void> threadh;
-		std::wstring          file_path    = path.wstring();
-		std::wstring          command_line = L"/install /quiet /norestart";
-		std::vector<WCHAR>    command_line_buf(command_line.size() + 1);
-		memcpy(command_line_buf.data(), command_line.data(), command_line.size() * sizeof(WCHAR));
-
-		si.cb             = sizeof(STARTUPINFOW);
-		sa.nLength        = sizeof(SECURITY_ATTRIBUTES);
-		sa.bInheritHandle = TRUE;
-
-		BOOL res = CreateProcessW(file_path.data(), command_line_buf.data(), &sa, &sa, TRUE, CREATE_UNICODE_ENVIRONMENT,
-								  NULL, NULL, &si, &pi);
-		processh = std::shared_ptr<void>(static_cast<void*>(pi.hProcess), helper_free_handle);
-		threadh  = std::shared_ptr<void>(static_cast<void*>(pi.hThread), helper_free_handle);
-
-		if (!res) {
-			DWORD error = GetLastError();
-
-			std::cerr << "Failed to launch installer, code " << error << "." << std::endl;
-			return 1;
-		}
-		std::cout << "Installer for MSVC " << msvc_version << " launched, waiting...";
-		std::cout.flush();
-
-		while (WaitForSingleObject(pi.hProcess, 5000) == WAIT_TIMEOUT) {
-			std::cout << ".";
-			std::cout.flush();
-		}
-		std::cout << std::endl;
-
-		DWORD exitcode = 0;
-		GetExitCodeProcess(pi.hProcess, &exitcode);
-
-		if (exitcode != 0) {
-			std::cerr << "There was an error installing or repairing the redistributable." << std::endl;
-			return 1;
-		}
-	}
-
-	// Clean up.
-	std::filesystem::remove(path);
-
-	std::cout << "Successfully updated MSVC " << msvc_version << " Redistributable to latest version." << std::endl;
-	return 0;
+	std::cerr << "Unknown Microsoft Visual C/C++ Redistributable version." << std::endl;
+	return 1;
 }
